46ebbe753cb7366c03e680a7f335a805c6b716b1
EC2 Default User <ec2-user@ip-172-31-85-45.ec2.internal>
2019-09-01 22:51:55 +0000

    My first commit

---
+++ lab1.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Labwork 1: Hello World</title>
+        <style>
+        body{
+            background-color: lightpink;
+        }
+        h1{
+            text-align: center;
+            color: navy;
+            border: 5px solid black;
+        }
+        p{
+            font-family: sans-serif;
+        }
+        </style>
+    </head>
+    <body>
+        <h1>Codie Brodeur</h1>
+        <p>Hello! My name is Codie Brodeur and I'm majoring in Graphic Design. 
+            I'm interested in knowing as much about the digital design space as possible.
+            My hobbies are learning skills that can further my career; like photography, 
+            web development and leadership skills. I'm so excited to learn PHP, I really 
+            wanted to take it last semester but it's only availible in the fall, so I've 
+            been excited for awhile. I even started learning the basics on Solo Learn.
+        </p>
+        
+        <p>This is my <a href="http://github.com/codieb1997/cosw30" target="_blank">
+            Github profile</a>
+        </p>
+        
+        <?php 
+        /*
+            lab1.php
+            Codie Brodeur
+            September 1,2019
+        */
+            print "<p>My favoite websites are <a href=\"https://www.youtube.com/\">
+            Youtube</a> or <a href=\"https://www.pinterest.com/\">Pinterest</a>. 
+            because they help me relax from the day and thats what I mostly use the 
+            internet for.</p>";
+            
+            print "<p>The hardest part of this project is that I can't get the 
+            preview to work and I don't know how to use Github or Heroku. The 
+            actual typing of HTML, CSS, and PHP I can do.</p>";
+            
+        ?>
+    </body>        
+</html>
--- labwork/lab1.php
+++ labwork/lab1.php
@@ -5,7 +5,9 @@
     </head>
     <body>
         <h1>Codie Brodeur</h1>
-        <p>Stare at wall turn and meow stare at wall some more meow again continue staring hunt anything that moves dont wait for the storm to pass, dance in the rain or massacre a bird in the living room and then look like the cutest and most innocent animal on the planet, go into a room to decide you didn't want to be in there anyway but twitch tail in permanent irritation for making sure that fluff gets into the owner's eyes. Nya nya nyan pee in the shoe wake up wander around the house making large amounts of noise jump on top of your human's bed and fall asleep again bird bird bird bird bird bird human why take bird out i could have eaten that. Warm up laptop with butt lick butt fart rainbows until owner yells pee in litter box hiss at cats ask to be pet then attack owners hand pelt around the house and up and down stairs chasing phantoms, for sleeps on my head. Lie in the sink all day litter box is life somehow manage to catch a bird but have no idea what to do next, so play with it until it dies of shock. Destroy the blinds catch mouse and gave it as a present pee in human's bed until he cleans the litter box but meow yet enslave the hooman. Sleeps on my head if it fits, i sits, lick butt and make a weird face, but fat baby cat best buddy little guy. Run outside as soon as door open climb leg, yet pee in the shoe, push your water glass </p>
+        <p>Hello! My name is Codie Brodeur and I'm majoring in Graphic Design. I'm intested in knowing as much about the digital design space as possible. MY hobbies are learning skills that can further my career; like photography, web development and leardership skills.I'm so excited to learn PHP I really wanted to take it last semester but its only availible in the fall, so I've been excited about this class for a while. I even started learning the basics on Solo Learn.
+        </p>
+        <p>This is my <a href="https://github.com/codieb1997/COSW30" target="_blank">Github profile</a></p>
     
     <?php
         echo '<a href=\"\">This is my PHP paragraph</a>';
@@ -15,6 +17,11 @@
 </html>
 
     <?php
+    /*
+        lab1.php
+        Codie Brodeur
+        August 30, 2019
+    */
         echo 'This is a string';
         echo 'Hello World!';
     ?>
